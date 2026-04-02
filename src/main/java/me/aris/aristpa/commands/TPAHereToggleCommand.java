f03372d9603df5e1b3bfa5857fcfc03ecdd290b6
nguyendzvennlmao <tqnmods111@gmail.com>
2026-04-02 16:16:23 +0700

    Create TPAHereToggleCommand.java

---
+++ src/main/java/me/aris/aristpa/commands/TPAHereToggleCommand.java
@@ -0,0 +1,41 @@
+package me.aris.aristpa.commands;
+
+import me.aris.aristpa.ArisTPA;
+import org.bukkit.entity.Player;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class TPAHereToggleCommand implements CommandExecutor {
+    private ArisTPA plugin;
+    
+    public TPAHereToggleCommand(ArisTPA plugin) {
+        this.plugin = plugin;
+    }
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            plugin.getMessageManager().sendMessage(null, "player-only");
+            return true;
+        }
+        
+        Player player = (Player) sender;
+        
+        if (!player.hasPermission("aristpa.tpaheretoggle")) {
+            plugin.getMessageManager().sendMessage(player, "no-permission");
+            return true;
+        }
+        
+        boolean current = plugin.getTPAManager().isTPAHereEnabled(player);
+        plugin.getTPAManager().setTPAHereEnabled(player, !current);
+        
+        if (!current) {
+            plugin.getMessageManager().sendMessage(player, "tpa-here-toggle-on");
+        } else {
+            plugin.getMessageManager().sendMessage(player, "tpa-here-toggle-off");
+        }
+        
+        return true;
+    }
+          }
